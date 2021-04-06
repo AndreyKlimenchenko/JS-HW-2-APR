12948133d066f0a4144d9cdf3bab4483730ad546
Андрей Клименченко <andreyklim@iMac-Andrej.local>
2021-04-06 16:44:02 +0300

    feat:add script

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ class AddressBook {
       this.searchResults = [];
   }
 
-  addNewContact(name,phone,email){
+  addNewContact(name,phone,email,address){
       this.data.push({
-        name: name,
-        phone: phone,
-        email:email
+        name,
+        phone,
+        email,
+        address
       });
       localStorage.setItem('contacts', JSON.stringify(this.data));
     }
@@ -18,7 +19,7 @@ class AddressBook {
       return this.data;
     }
   search(searchTerm){
-  if(this.data.length){
+    if(this.data.length){
       for(let i=0;i<this.data.length;i++){
       if(this.data[i].name.toLowerCase() == searchTerm.toLowerCase()){
           this.searchResults.push(this.data[i]);
@@ -35,6 +36,15 @@ class AddressBook {
   init() {
       this.returnAll();
   }
+  removeContact(name) {
+    const contacts = localStorage.getItem('contacts');
+    let parsedContacts = contacts ? JSON.parse(contacts) : [];
+    this.data = parsedContacts.filter(contact => {
+      return contact.name.toLowerCase() !== name.toLowerCase();
+    })
+    console.log('!!!!');
+    localStorage.setItem('contacts', JSON.stringify(this.data));
+  }
 }
 
 const newApp = new AddressBook();
@@ -42,7 +52,7 @@ newApp.init();
 
 let form  = document.getElementById('contact');
 form.addEventListener('submit', function(){
-  newApp.addNewContact(form.person.value,form.phone.value,form.email.value);
+  newApp.addNewContact(form.person.value,form.phone.value,form.email.value,form.address.value);
   form.reset();
   event.preventDefault();
 });
@@ -55,7 +65,7 @@ searchForm.addEventListener('submit', function(){
   if(results.length>0){
   
   for(let i = 0;i<results.length;i++){
-      document.getElementById('results').innerHTML += '<div class="contact-item">Name:'+results[i].name+'<br>Phone:'+results[i].phone+'<br>Email:'+results[i].email+'</div><hr>';
+      document.getElementById('results').innerHTML += '<div class="contact-item">Name:'+results[i].name+'<br>Phone:'+results[i].phone+'<br>Email:'+results[i].email+'<br>Address:'+results[i].address+'</div><hr>';
   }
   } else{
   document.getElementById('results').innerHTML += '<div class="contact-item">Не найден</div><hr>';
@@ -69,7 +79,7 @@ document.getElementById('js-show-all').addEventListener('click', function(){
   const contacts = newApp.returnAll();
   if(contacts.length>0){
       for(let i = 0;i<contacts.length;i++){
-      document.getElementById('show-panel').innerHTML += '<div class="contact-item">Name:'+contacts[i].name+'<br>Phone:'+contacts[i].phone+'<br>Email:'+contacts[i].email+'</div><hr>';
+      document.getElementById('show-panel').innerHTML += '<div class="contact-item">Name:'+contacts[i].name+'<br>Phone:'+contacts[i].phone+'<br>Email:'+contacts[i].email+'<br>Address:'+contacts[i].address+'</div><hr>';
       }
   }else{
       document.getElementById('show-panel').innerHTML += '<div class="contact-item">У вас нет контактов. Добавьте!</div><hr>';
@@ -92,3 +102,7 @@ document.getElementById('js-add-new').addEventListener('click', function(){
   document.getElementById('search-panel').style.display = 'none';
   document.getElementById('contact-panel').style.display = 'block';
 });
+document.getElementById('del-btn').addEventListener('click', function(){
+  newApp.removeContact(searchForm.search.value);
+  document.getElementById('results').innerHTML = '<div class="contact-item">Контакт удален</div><hr>';
+})
